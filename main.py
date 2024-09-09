83e081fed894fd84ad463eafc767183d7ed88510
Tom Donahue <>
2024-09-09 19:57:54 -0400

    got the merge command working

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ dataframes = {
 dataframes['file1'] = pd.read_csv('file1.csv')
 dataframes['file2'] = pd.read_csv('file2.csv')
 
-
-
 def parse_commands(commands:str):
-    arguments = commands.split()
-    command = arguments[0]
-    arguments = [args for args in arguments[1:] if args != '']
+    split_commands = commands.split()
+    command = split_commands[0]
+    arguments = [args for args in split_commands[1:] if args != '']
     print(arguments)
 
     return command, arguments
 
-def merge(arguments):
-    file1,file2,how,on = arguments
-
-    print(on)
-    if how =='left':
-        suffixes = (None,'_duplicate')
+def merge_left(arguments):
+    file1,file2,on = arguments[0:3]
+    if len(arguments) > 3:
+        cols = arguments[3:]
+        cols.append(on)
     else:
-        suffixes = ('_duplicate',None)
+        cols = list(dataframes[file2].columns.values)
 
-    file = pd.merge(dataframes[file1],dataframes[file2],how=how,on=on,suffixes=suffixes)
+    suffixes = (None,'_duplicate')
+
+    file = pd.merge(dataframes[file1],dataframes[file2][cols],how='left',on=on,suffixes=suffixes)
     
     cols = [col for col in file.columns if col.endswith("_duplicate")]
     file.drop(columns=cols, inplace=True)
@@ -36,10 +35,14 @@ def merge(arguments):
 
 
 commands = {
-    'merge': merge
+    'merge': merge_left
 }
 
 input = input("tell me what you want ")
 
 command, args = parse_commands(input)
-commands[command](args)
+try:
+    commands[command](args)
+except ValueError:
+    print("Incorrect Arguments Provided!")
+
