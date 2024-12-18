1b1b123313e69fe432c48a1a48202f5a1b0ae777
Tom Donahue <>
2024-12-18 17:34:58 -0500

    Added a TODO for documentation

--- src/commands/factory.py
+++ src/commands/factory.py
@@ -31,6 +31,9 @@ def generate_cmd_and_args(model: Model, cmd_str: str, args: list) -> tuple[Comma
     if cmd_str not in COMMANDS:
         raise Exception(f"Command {cmd_str} does not exist.")
     expected_args = len(inspect.signature(COMMANDS[cmd_str]['args']).parameters)-1
+    # TODO: Add documentation to notate the use case here. Any command can use a catch all list as the final argument. 
+    # If you insert arguments that are greater than the number of parameters in the command, the final arguments are converted to a list by the factory
+    # before being injected into the command
     if expected_args < len(args):
         list_args = [arg for arg in args[expected_args-1:]]
         args = args[:expected_args-1]
